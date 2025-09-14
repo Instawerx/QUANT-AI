a26925810acba38f2776c9eee9a44122b02d71e5
Samuel Eremo <vrdivebar@gmail.com>
2025-09-14 02:53:55 +0000

    That did not work, We need to see full chart it is currently folded.

--- src/components/trading-chart.tsx
+++ src/components/trading-chart.tsx
@@ -7,12 +7,6 @@ function TradingViewWidget() {
   useEffect(
     () => {
       if (!container.current) return;
-      
-      // Clean up existing scripts if any
-      const scripts = container.current.getElementsByTagName('script');
-      for (let i = 0; i < scripts.length; i++) {
-        scripts[i].remove();
-      }
 
       const script = document.createElement("script");
       script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
@@ -35,14 +29,13 @@ function TradingViewWidget() {
         "support_host": "https://www.tradingview.com"
       }`;
       
+      // Clear the container before appending the script
+      container.current.innerHTML = '';
       container.current.appendChild(script);
 
       return () => {
         if (container.current) {
-            const scripts = container.current.getElementsByTagName('script');
-            for (let i = 0; i < scripts.length; i++) {
-                scripts[i].remove();
-            }
+            container.current.innerHTML = '';
         }
       }
     },
@@ -51,7 +44,7 @@ function TradingViewWidget() {
 
   return (
     <div className="tradingview-widget-container" ref={container} style={{ height: "700px", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+      <div className="tradingview-widget-container__widget" style={{ height: "100%", width: "100%" }}></div>
       <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
     </div>
   );
